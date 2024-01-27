4e292266eb29a4abd37ad3c3ec6a64b1438f8656
yavrum13 <yuval.avrum@gmail.com>
2024-01-27 16:25:36 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = "Yuval Avrum";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
+
